b025579c2cc4f231a56746ce6c9dc5e9bbc57d49
Sumanth Akula <sumoa29@gmail.com>
2024-09-24 01:23:34 -0400

    Initial Commit

---
+++ CouchPotato/CouchPotatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouchPotatoApp.swift
+//  CouchPotato
+//
+//  Created by Sumo Akula on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CouchPotatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
