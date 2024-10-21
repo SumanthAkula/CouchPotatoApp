4467436561d36f848a2136283ade4a0d0d90a3ed
Sumanth Akula <sumoa29@gmail.com>
2024-10-21 10:06:53 -0400

    Added new endpoint for getting episodes

--- CouchPotato/Service/TvMazeApiService.swift
+++ CouchPotato/Service/TvMazeApiService.swift
@@ -9,10 +9,68 @@ import Foundation
 import SwiftUI
 
 
-@MainActor class TvMazeApiService: ObservableObject {
-    private let baseURL: String = "https://api.tvmaze.com"
+class TvMazeApiService {
+    private static let baseURL: String = "https://api.tvmaze.com"
     
-    func searchForShow(query: String, completion: @escaping (Result<[TvShow], Error>) -> Void) {
+    static func getEpisode(link: String, completion: @escaping (Result<TvEpisode, Error>) -> Void) {
+        guard let url = URL(string: link) else {
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            if let data = data {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                
+                do {
+                    let result = try decoder.decode(TvEpisode.self, from: data)
+                    completion(.success(result))
+                } catch (let error) {
+                    completion(.failure(error))
+                }
+            }
+        }.resume()
+    }
+    
+    static func getEpisode(id: Int) {
+        return
+    }
+    
+    static func getEpisodes(for show: TvShow, completion: @escaping (Result<[TvEpisode], Error>) -> Void) {
+        guard let queryURL = URL(string: "\(baseURL)/shows/\(show.id)/episodes?specials=1") else {
+            return
+        }
+        
+        let request = URLRequest(url: queryURL)
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            if let data = data {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+            
+                do {
+                    let result = try decoder.decode([TvEpisode].self, from: data)
+                    completion(.success(result))
+                } catch (let error) {
+                    completion(.failure(error))
+                }
+            }
+        }.resume()
+    }
+    
+    static func searchForShow(query: String, completion: @escaping (Result<[TvShow], Error>) -> Void) {
         guard let queryURL = URL(string: "\(baseURL)/search/shows?q=\(query)") else {
             return
         }
