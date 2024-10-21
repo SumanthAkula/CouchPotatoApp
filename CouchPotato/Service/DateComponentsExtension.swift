6fb9dbb92377f5981891639d8efc965e57e3a3b4
Sumanth Akula <sumoa29@gmail.com>
2024-10-21 10:04:35 -0400

    created extension to store DateComponents day values

---
+++ CouchPotato/Service/DateComponentsExtension.swift
@@ -0,0 +1,20 @@
+//
+//  DateComponentsExtension.swift
+//  CouchPotato
+//
+//  Created by Sumo Akula on 10/19/24.
+//
+
+import Foundation
+
+extension DateComponents {
+    enum Day: Int {
+        case sunday = 1
+        case monday = 2
+        case tuesday = 3
+        case wednesday = 4
+        case thursday = 5
+        case friday = 6
+        case saturday = 7
+    }
+}
